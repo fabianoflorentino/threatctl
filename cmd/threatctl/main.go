aa693f3a4b4b8adb670627767fcc2fd6583b1f9c
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-03-28 14:37:46 -0300

    feat: add summary command to print JSON summary of PCAP files

--- cmd/threatctl/main.go
+++ cmd/threatctl/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -31,8 +32,29 @@ var analyzeCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(analyzeCmd)
+	rootCmd.AddCommand(summaryCmd)
 }
 
+var summaryCmd = &cobra.Command{
+	Use:   "summary [pcap file]",
+	Short: "Print a JSON summary of a PCAP file",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		path := args[0]
+		topN, _ := cmd.Flags().GetInt("top")
+		s, err := pcap.SummarizePCAP(path, topN)
+		if err != nil {
+			return err
+		}
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		return enc.Encode(s)
+	},
+}
+
+func init() {
+	summaryCmd.Flags().IntP("top", "t", 5, "Top N items for IPs/ports")
+}
 func main() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
